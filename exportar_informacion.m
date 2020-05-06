0497087621d6813d8d762c4d37851fa3ee958780
HeavyRain350 <rain34188@yahoo.com>
2020-05-05 19:56:43 -0500

    Se adjuntan las 63 imagenes separadas del articulo de redondez, los resultados fueron corridos con 20 armonicos y 20 de distancia maxima

--- exportar_informacion.m
+++ exportar_informacion.m
@@ -5,7 +5,7 @@ function exportar_informacion
     jpgfiles = dir(fullfile(images,'\*.jpg'));
     
     n = numel(jpgfiles); %Numero de imagenes en el directorio im_pruebas
-    N = 45;
+    N = 20;
 
     cellExcentricity = zeros([n N]);
     roundClassifications = cell([n,1]);
@@ -13,15 +13,15 @@ function exportar_informacion
     for imnum = 1:n
         imname = jpgfiles(imnum).name;
         img_route = fullfile(images,imname);
-       % excen = armonicosFourierEliptico(N,img_route);
-        roundness = inscribedCircles_3(img_route,N,.1,20,imname);
+        excen = armonicosFourierEliptico(N,img_route);
+        roundness = inscribedCircles_4(img_route,N,20,imname);
         
-        rClass = fix(roundness*10)/10; %Se obtiene el valor del primer decimal, debido a que solo existen 9 clases 0.1 - 0.9
+        rClass = round(roundness*10)/10; %Se obtiene el valor del primer decimal, debido a que solo existen 9 clases 0.1 - 0.9
         roundClassifications{imnum} = rClass;
-     %   cellExcentricity(imnum,:) = excen';
+        cellExcentricity(imnum,:) = excen';
     end
     
-    finalCell = [classifications  num2cell(cellExcentricity)]; %Combinamos las celdas de excentricidad y clasificaciones
+    finalCell = [roundClassifications  num2cell(cellExcentricity)]; %Combinamos las celdas de excentricidad y clasificaciones
     tableExcen = cell2table(finalCell);
     writetable(tableExcen,filename,'Sheet',1,'Range','A1')
 end
