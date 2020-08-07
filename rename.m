394f0a5dedc2dc25b7cd0249e27df2c14f276bc2
Gamaliel <gamalielmch@gmail.com>
2020-08-07 18:01:13 -0500

    actualización base de datos

--- rename.m
+++ rename.m
@@ -81,41 +81,73 @@
 %     end
 % end
 
-%
-am=63681;
-%pathi=[pwd,'\all\'];
-pathi='C:\Users\Gama\Documents\new_images\';
+
+
+
+% am=63681;
+% %pathi=[pwd,'\all\'];
+% pathi='C:\Users\Gama\Documents\new_images\';
+% f=dir([pathi,'*.jpg']);
+% file={};
+% for i=1:length(f)
+%     file(i)={f(i).name};
+% end
+% path=pathi;
+% ini=0;
+% for i=1:length(file)
+%     im = imread([path, file{i}]);
+%     im = im2bw(im(:,:,1));
+%     stats=regionprops(im,'Image');
+%     i
+%     length(stats)
+%     for j=1:length(stats)
+%         j
+%         im=stats(j).Image;
+%         im=padarray(im,[20 20],'both');
+%         area=sum(im(:));
+%         area=area/am;
+%         if area<0.61
+%             im=imresize(im,sqrt(1/area));
+%             im=regionprops(im,'Image');
+%             im=im(1).Image;
+%             im=padarray(im,[20 20],'both');
+%             % imwrite(im, ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}]);
+%         end
+%         imwrite(im, ['C:\Users\Gama\Documents\new_images\database2\', num2str(j+ini),'.jpg']);
+%         %      movefile([path, file{i}], ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}])
+%     end
+%     ini=j+ini;
+%     
+%     
+% end
+
+
+pathi='C:\Users\Gama\Documents\new_images\database\';
+f=dir([pathi,'*.jpg']);
+file={};
+for i=1:length(f)
+    file(i)={f(i).name};
+end
+ini=length(file);
+
+pathi='C:\Users\Gama\Documents\new_images\database2\';
 f=dir([pathi,'*.jpg']);
 file={};
 for i=1:length(f)
     file(i)={f(i).name};
 end
 path=pathi;
-ini=0;
-for i=1:length(file)
-    im = imread([path, file{i}]);
-    im = imbinarize(im(:,:,1));
-    stats=regionprops(im,'Image');
-    i
-    length(stats)
-    for j=1:length(stats)
-        im=stats(j).Image;
-        im=padarray(im,[20 20],'both');
-        area=sum(im(:));
-        area=area/am;
-        if area<0.61
-            im=imresize(im,sqrt(1/area));
-            im=regionprops(im,'Image');
-            im=im(1).Image;
-            im=padarray(im,[20 20],'both');
-            % imwrite(im, ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}]);
-        end
-        imwrite(im, ['C:\Users\Gama\Documents\new_images\database\', num2str(j+ini),'.jpg']);
-        %      movefile([path, file{i}], ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}])
-    end
-    ini=j+ini;
-    
-    
+
+for i=1:length(indi)
+    im = imread([path, file{indi(i)}]);
+
+        imwrite(im, ['C:\Users\Gama\Documents\new_images\database\', num2str(i+ini),'.jpg']);
+      %    movefile([path, file{i}], ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}])
 end
+
+    
+
+
+
 
 
