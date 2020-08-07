0204330b32cef5bfc077b70940c61592b3a453a2
Gamaliel <gamalielmch@gmail.com>
2020-08-07 14:44:12 -0500

    eliminando archivos inncesarios

--- rename.m
+++ rename.m
@@ -1,8 +1,8 @@
-% 
-% 
+%
+%
 % cont=1;
 % for jj=1:14
-%     
+%
 %     pathi=[pwd,'\database',num2str(jj),'\'];
 %     f=dir([pathi,'*.jpg']);
 %     file={};
@@ -10,8 +10,8 @@
 %         file(i)={f(i).name};
 %     end
 %     path=pathi;
-%     
-%    
+%
+%
 %     for i=1:length(file)
 %         im = imread([path, file{i}]);
 %         im = imbinarize(im(:,:,1));
@@ -26,16 +26,17 @@
 
 
 
-% 
-% am=57681;
-% pathi=[pwd,'\all\'];
+% %
+% am=63681;
+% %pathi=[pwd,'\all\'];
+% pathi='C:\Users\Gama\Documents\new_images\';
 % f=dir([pathi,'*.jpg']);
 % file={};
 % for i=1:length(f)
 % file(i)={f(i).name};
 % end
 % path=pathi;
-% 
+%
 % for i=1:length(file)
 %     im = imread([path, file{i}]);
 %     im = imbinarize(im(:,:,1));
@@ -49,32 +50,72 @@
 %     im=regionprops(im,'Image');
 %     im=im(1).Image;
 %     im=padarray(im,[20 20],'both');
-%     imwrite(im, ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}]);
+%    % imwrite(im, ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}]);
 %     end
+%     imwrite(im, ['C:\Users\Gama\Documents\new_images\temporal\', num2str(i),'.jpg']);
 % %      movefile([path, file{i}], ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}])
-%      
+%
+% end
+%
+
+
+% cont=1;
+% for jj=1:9
+%
+%     pathi=[pwd,'\class',num2str(jj),'\'];
+%     f=dir([pathi,'*.jpg']);
+%     file={};
+%     for i=1:length(f)
+%         file(i)={f(i).name};
+%     end
+%     path=pathi;
+%
+%
+%     for i=1:length(file)
+%         im = imread([path, file{i}]);
+% %         im = imbinarize(im(:,:,1));
+% %         im=regionprops(im,'Image');
+% %         im=im(1).Image;
+%         imwrite(im,['C:\Users\Gama\Documents\Github\Morphology\allclass\',num2str(cont),'.jpg']);
+%         cont=cont+1;
+%     end
 % end
 
-
-
-cont=1;
-for jj=1:9
-    
-    pathi=[pwd,'\class',num2str(jj),'\'];
-    f=dir([pathi,'*.jpg']);
-    file={};
-    for i=1:length(f)
-        file(i)={f(i).name};
-    end
-    path=pathi;
-    
-   
-    for i=1:length(file)
-        im = imread([path, file{i}]);
-%         im = imbinarize(im(:,:,1));
-%         im=regionprops(im,'Image');
-%         im=im(1).Image;
-        imwrite(im,['C:\Users\Gama\Documents\Github\Morphology\allclass\',num2str(cont),'.jpg']);
-        cont=cont+1;
-    end
+%
+am=63681;
+%pathi=[pwd,'\all\'];
+pathi='C:\Users\Gama\Documents\new_images\';
+f=dir([pathi,'*.jpg']);
+file={};
+for i=1:length(f)
+    file(i)={f(i).name};
 end
+path=pathi;
+ini=0;
+for i=1:length(file)
+    im = imread([path, file{i}]);
+    im = imbinarize(im(:,:,1));
+    stats=regionprops(im,'Image');
+    i
+    length(stats)
+    for j=1:length(stats)
+        im=stats(j).Image;
+        im=padarray(im,[20 20],'both');
+        area=sum(im(:));
+        area=area/am;
+        if area<0.61
+            im=imresize(im,sqrt(1/area));
+            im=regionprops(im,'Image');
+            im=im(1).Image;
+            im=padarray(im,[20 20],'both');
+            % imwrite(im, ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}]);
+        end
+        imwrite(im, ['C:\Users\Gama\Documents\new_images\database\', num2str(j+ini),'.jpg']);
+        %      movefile([path, file{i}], ['C:\Users\Gama\Documents\Github\Morphology\all\', file{i}])
+    end
+    ini=j+ini;
+    
+    
+end
+
+
